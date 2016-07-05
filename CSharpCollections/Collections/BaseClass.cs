360bf299a4ca932a8e9d59682921bd9293a796d2
Paul <paul.j.millar.1971@gmail.com>
2016-07-05 14:26:46 +0100

    Initial Create - Added ConcurrentDictionary Example

---
+++ CSharpCollections/Collections/BaseClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCollections.Collections
+{
+    public class BaseClass
+    {
+        #region PRINT METHODS
+        public static void PrintTitle(string title)
+        {
+            string divider = new String('*', 70);
+            Console.WriteLine("");
+            Console.WriteLine(divider);
+            Console.WriteLine(title);
+            Console.WriteLine(divider);
+        }
+
+        public static void PrintSubTitle(string title)
+        {
+            string divider = new String('=', 70);
+            Console.WriteLine("");
+            Console.WriteLine(divider);
+            Console.WriteLine(title);
+            Console.WriteLine(divider);
+        }
+
+        public static void PrintTime(double time)
+        {
+            string divider = new String('-', 70);
+            Console.WriteLine(divider);
+            Console.WriteLine($"Execution time = {time} seconds");
+            Console.WriteLine(divider);
+        }
+        #endregion
+    }
+}
---
+++ CSharpCollections/Collections/Concurrent/ConcurrentDictionaryCollection.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading;
+
+namespace CSharpCollections.Collections.Concurrent
+{
+    class ConcurrentDictionaryCollection : BaseClass
+    {
+        static ConcurrentDictionary<int, string> cd = new ConcurrentDictionary<int, string>();
+
+        public static void Run()
+        {
+            PrintTitle("CONCURRENT DICTIONARY");
+            ThreadTest();
+
+            cd.Clear();
+
+            TryMethods();
+            AddOrUpdate();
+        }
+
+        private static void ThreadTest()
+        {
+            PrintTitle("THREADING");
+            Thread thread1 = new Thread(new ThreadStart(TryAddMany));
+            Thread thread2 = new Thread(new ThreadStart(TryAddMany));
+
+            thread1.Name = "Thread1";
+            thread2.Name = "Thread2";
+
+            thread1.Start();
+            thread1.Join();
+
+            thread2.Start();
+        }
+
+        private static void TryAddMany()
+        {
+            PrintSubTitle($"THREAD ID: {Thread.CurrentThread.Name}");
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                cd.TryAdd(i, i.ToString());
+            }
+
+            Console.WriteLine("Average: {0}", cd.Keys.Average());
+        }
+
+        private static void TryMethods()
+        {
+            PrintSubTitle("TRY METHODS");
+
+            int numFailures = 0;
+
+            /****************************************************************************************/
+            // TRY ADD
+            /****************************************************************************************/
+            // This should work
+            if (cd.TryAdd(1, "one"))
+            {
+                Console.WriteLine("CD.TryAdd() 'one' succeeded");
+            }
+            else
+            {
+                Console.WriteLine("CD.TryAdd() 'one' failed");
+                numFailures++;
+            }
+
+            // This shouldn't work -- key 1 is already in use
+            if (cd.TryAdd(1, "uno"))
+            {
+                Console.WriteLine("CD.TryAdd() succeeded when it should have failed");
+                numFailures++;
+            }
+            else
+            {
+                Console.WriteLine("CD.TryAdd() failed (this is suppose to happen)");
+            }
+
+            /****************************************************************************************/
+            // TRY UPDATE
+            /****************************************************************************************/
+            // Now change the value for key 1 from "one" to "uno" -- should work
+            if (!cd.TryUpdate(1, "uno", "one"))
+            {
+                Console.WriteLine("CD.TryUpdate() failed when it should have succeeded");
+                numFailures++;
+            }
+
+            // Try to change the value for key 1 from "eine" to "one" 
+            //    -- this shouldn't work, because the current value isn't "eine"
+            if (cd.TryUpdate(1, "one", "eine"))
+            {
+                Console.WriteLine("CD.TryUpdate() succeeded when it should have failed");
+                numFailures++;
+            }
+            else
+            {
+                Console.WriteLine("CD.TryUpdate() failed (this is suppose to happen)");
+            }
+
+            /****************************************************************************************/
+            // TRY REMOVE
+            /****************************************************************************************/
+            // Remove key/value for key 1.  Should work.
+            string value1;
+            if (!cd.TryRemove(1, out value1))
+            {
+                Console.WriteLine("CD.TryRemove() failed when it should have succeeded");
+                numFailures++;
+            }
+            else
+            {
+                Console.WriteLine($"Removed value: {value1}");
+            }
+
+            // Remove key/value for key 1.  Shouldn't work, because I already removed it
+            string value2;
+            if (cd.TryRemove(1, out value2))
+            {
+                Console.WriteLine("CD.TryRemove() succeeded when it should have failed");
+                numFailures++;
+            }
+
+            // If nothing went wrong, say so
+            if (numFailures == 0)
+            {
+                Console.WriteLine("OK!");
+            }
+
+        }
+
+        private static void AddOrUpdate()
+        {
+            /****************************************************************************************/
+            // TRY ADD
+            /****************************************************************************************/
+            // Add a new key/value, This should work
+            if (cd.TryAdd(1, "one"))
+            {
+                Console.WriteLine("CD.TryAdd() 'one' succeeded");
+            }
+
+            /****************************************************************************************/
+            // ADD OR UPDATE
+            /****************************************************************************************/
+            // Now change the value for key 1 from "one" to "eon" -- should work
+            string oldValue = "one";
+            string newValue = "eon";
+            cd.AddOrUpdate(1, oldValue, (k, v) => newValue);
+
+            /****************************************************************************************/
+            // TRY GET
+            /****************************************************************************************/
+            // Now get the value for key 1
+            var val = "";
+            var first = cd.TryGetValue(1, out val);
+            Console.WriteLine(val);
+
+        }
+    }
+}
