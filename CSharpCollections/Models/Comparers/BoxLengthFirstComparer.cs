fef14fcc2fafd3c64d11a9b680f3d7f37cb55b6e
Paul <paul.j.millar.1971@gmail.com>
2016-07-05 19:16:03 +0100

    Moved BoxLengthFirstComparer class

--- CSharpCollections/Models/Comparers/BoxLengthFirstComparer.cs
+++ CSharpCollections/Models/Comparers/BoxLengthFirstComparer.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
-namespace CSharpCollections.Models
+namespace CSharpCollections.Models.Comparers
 {
     public class BoxLengthFirstComparer : Comparer<Box>
     {
